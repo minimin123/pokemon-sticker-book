92644ee10b523b61673f46c8b8a1650436eac93d
minimin123 <kmmin727@gmail.com>
2022-08-19 13:42:00 +0900

    [fix] 이미지 경로 오류 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,13 @@ import styled from '@emotion/styled';
 
 import OgTags from '../src/components/ogTags/ogTags';
 
-import Pokeball from '../src/assets/pokeball-icon.png';
-
 export default function Home() {
   return (
     <Wrap>
       <OgTags
         title="POKEMON STICKER BOOK"
         description="search your pokemon sticker!"
-        img={Pokeball.src}
+        img="/pokeball-icon.png"
       />
       <h1>Search Pokemon ID or name and collect pokemon-sticker!</h1>
     </Wrap>
